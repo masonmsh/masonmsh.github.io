e63eb9b95c54bfcbdf460c57a514c81003a6c3da
msh <hgsqtdsswk@163.com>
2020-12-05 17:03:16 +0800

    js

---
+++ base.js
@@ -0,0 +1,11 @@
+function changebase() {
+    burl = document.getElementById("burl");
+    logo = document.getElementById("logo");
+    if (burl.href.match("https")) {
+        burl.href = "file:///D:/space/git/masonmsh.github.io/";
+        logo.src = "source/logo.jpg";
+    } else {
+        burl.href = "https://masonmsh.github.io/";
+        logo.src = "favicon.ico";
+    }
+}
