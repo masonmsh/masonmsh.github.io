a2dbaddf8ad570206bb7224f27794a69fc312431
msh <hgsqtdsswk@163.com>
2021-05-26 16:33:09 +0800

    js

--- doc/css/base.js
+++ doc/css/base.js
@@ -2,10 +2,10 @@ function changebase() {
     burl = document.getElementById("burl");
     logo = document.getElementById("logo");
     if (burl.href.match("https")) {
-        burl.href = "file:///D:/code/git/masonmsh.github.io/";
         logo.src = "source/logo.jpg";
+        burl.href = "file:///D:/code/git/masonmsh.github.io/";
     } else {
-        burl.href = "https://masonmsh.github.io/";
         logo.src = "favicon.ico";
+        burl.href = "https://masonmsh.github.io/";
     }
 }
